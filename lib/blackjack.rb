d287e04135c3dbd23a9333995d57ecbbc5ef647b
Deijah Price <deijahwilson1@gmail.com>
2020-03-06 03:33:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,6 +51,7 @@ end
 
 def invalid_command
   # code invalid_command here
+  puts ""
 end
 
 #####################################################
@@ -59,5 +60,12 @@ end
 
 def runner
   # code runner here
+   welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
