c4fbafec68d403b9c5c27ca3efb2cd43f400c93d
Deijah Price <deijahwilson1@gmail.com>
2020-03-06 03:31:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,6 +29,11 @@ end
 
 def initial_round
   # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  sum = card_one + card_two
+  display_card_total(sum)
+  sum
 end
 
 def hit?
